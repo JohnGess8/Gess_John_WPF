32ae91b0f25fc6d354beeed54dec53744c6cf371
johngess <john.gess8@fullsail.edu>
2013-05-24 22:10:44 -0400

    Conditional Industry

---
+++ Conditionals_Industry/js/Conditional_Industry.js
@@ -0,0 +1,12 @@
+//Conditional Industry Week 3 Assignment Conditionals John Gess May 23, 2013
+
+var paycheck = 800;
+
+// if paycheck is over 700 all my bills will be paid
+if(paycheck >700){
+    console.log ("Bills are paid!");
+}else{
+    console.log ("Not enough for bills");
+}*/
+
+    (paycheck > 800) ? congsole.log("Bills are paid!") : console.log ("Not enough for bills");
