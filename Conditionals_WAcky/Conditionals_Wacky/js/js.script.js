a26c1fbc9d428d6b29a750bb8ecaff887063f8c6
johngess <john.gess8@fullsail.edu>
2013-05-24 22:11:48 -0400

    Conditonals Wacky

---
+++ Conditionals_WAcky/Conditionals_Wacky/js/js.script.js
@@ -0,0 +1,12 @@
+// Conditional Wacky John Gess Week 3 Assignment May 23, 2013
+
+var biweeklyCheck = 900
+var vetBills = 200.88
+var yearBonus = true;
+
+// if the price of the vet bills are less then our biweekyCheck AND if our check is over 900
+if(vetBills < biweeklyCheck || yearBonus === true{
+    console.log("I can pay my dog's vet bills");
+}else{
+    console.log("No healthy dog");
+}
