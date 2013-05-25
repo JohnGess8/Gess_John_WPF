fb174693696cf399d2ce77d1ccb0fb054adde939
johngess <john.gess8@fullsail.edu>
2013-05-24 22:11:17 -0400

    Conditional Personal

---
+++ Conditionals_Personal/js/Conditional_Logic.js
@@ -0,0 +1,9 @@
+// Conditional Logic  John Gess Week 3 Assignment May 23, 2013
+
+var newTattoo = true;
+
+// if the tattoo is less then 400, print to console "It will be done."
+if(newTattoo){
+    //code perform if condition is true
+    console.log ("I'm getting the tattoo!!");
+}
