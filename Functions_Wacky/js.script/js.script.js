3a05dadcd21083195098b0e08fa58ebe55ad142f
johngess <john.gess8@fullsail.edu>
2013-05-29 19:08:29 -0400

    Functions Wacky

---
+++ Functions_Wacky/js.script/js.script.js
@@ -0,0 +1,11 @@
+// Week 4 Functions May 29, 2013 John Gess WPF section 01
+
+var calcArea = function(w,h){
+    var area = w = h;
+    return area;
+
+}
+
+var a;
+a = calcArea(4583, 4783);
+console.log(a);
