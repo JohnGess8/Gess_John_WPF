545ff8eb0e04c070c74bad739a7df5f71b08d49c
johngess <john.gess8@fullsail.edu>
2013-05-29 19:08:10 -0400

    Functions Personal

---
+++ Functions_Personal/js/js.script.js
@@ -0,0 +1,10 @@
+// Functions_Personal John Gess May 31, 2013 Week 4 Assignment
+
+function gradYear (myAge){
+    var gradYear = myAge + 2016;
+    console.log("John Gess will be " +  gradYear + " graduation date. ");
+
+}
+gradYear(3);
+gradYear(4);
+
