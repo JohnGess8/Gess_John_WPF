5b526ca6ec048680621d366d97a12a9cb384408c
johngess <john.gess8@fullsail.edu>
2013-05-29 19:07:48 -0400

    Week 4 Industry

---
+++ Functions_Industry/js/js.script.js
@@ -0,0 +1,11 @@
+// Functions_Industry John Gess Week 4 Assignment May 31, 2013
+
+function calArea(){
+    var width = 27329
+    var height = 38883
+    var area = width * height;
+    console.log(area);
+}
+calArea();
+
+
